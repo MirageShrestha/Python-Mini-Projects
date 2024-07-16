cabe333d279ed87c10282a955a12cff81e5aac6d
MirageShrestha <128497517+MirageShrestha@users.noreply.github.com>
2024-07-16 22:03:15 +0545

    Add files via upload

---
+++ KBC case.py
@@ -0,0 +1,20 @@
+# Question=["Who am I?"]
+# Answer=["Mirage","Avishek","Prekshya","Jesus"]
+# print(Question)
+# print(Answer)
+# ans=input("choose the correct answer:")
+# if ans == Answer[0]:
+#     print("your answer is correct")
+# else:
+#     print("you are wrong person") 
+Question=["Who am I?"]
+Answer=["Mirage","Avishek","Prekshya","Jesus"]
+print(Question)
+print(Answer)
+operation=input("Enter your choice(a,b,c,d):")
+if operation=="a":
+    print(Answer[0])
+    print("your answer is correct")
+    print("you have won 11$")
+else:
+    print("you are wrong")
